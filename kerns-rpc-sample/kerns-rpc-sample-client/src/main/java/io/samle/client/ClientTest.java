52c2afbf2a18e71aa42e0cf1879950e06416878c
kk <realmall20@protonmail.com>
2020-07-06 08:25:38 +0800

    m

--- kerns-rpc-sample/kerns-rpc-sample-client/src/main/java/io/samle/client/ClientTest.java
+++ kerns-rpc-sample/kerns-rpc-sample-client/src/main/java/io/samle/client/ClientTest.java
@@ -1,11 +1,14 @@
-package io.samle.client.service;
+package io.samle.client;
 
 import io.kerns.rpc.client.discover.Discover;
 import io.kerns.rpc.client.discover.MockDiscover;
 import io.kerns.rpc.client.factory.JdkProxyFactory;
 import io.kerns.rpc.client.net.NetClient;
 import io.kerns.rpc.client.net.SocketNetClient;
+import io.kerns.rpc.common.protocol.JavaSerializableMessageProtocol;
+import io.kerns.rpc.common.protocol.MessageProtocol;
 import io.samle.bean.User;
+import io.samle.client.service.UserService;
 
 /**
  * @author xiaohei
@@ -15,9 +18,12 @@ public class ClientTest {
 
     public static void main(String[] args) {
         NetClient netClient = new SocketNetClient();
-        Discover discover=new MockDiscover();
+        Discover discover = new MockDiscover();
+        MessageProtocol protocol = new JavaSerializableMessageProtocol();
         JdkProxyFactory proxyFactory = new JdkProxyFactory();
         proxyFactory.setNetClient(netClient);
+        proxyFactory.setDiscover(discover);
+        proxyFactory.setProtocol(protocol);
         UserService userService = proxyFactory.getProxy(UserService.class);
         User user = userService.getById(12);
         System.out.println(user);
--- kerns-rpc-sample/kerns-rpc-sample-server/src/main/java/io/sample/server/UserService.java
+++ kerns-rpc-sample/kerns-rpc-sample-server/src/main/java/io/sample/server/UserService.java
@@ -1,8 +1,17 @@
 package io.sample.server;
 
+import io.samle.bean.User;
+
 /**
  * @author xiaohei
  * @create 2020-07-05 下午10:11
  **/
 public class UserService {
+
+    public User getUserById(Integer id){
+        User user=new User();
+        user.setName("test");
+        user.setId(id);
+        return new User();
+    }
 }
