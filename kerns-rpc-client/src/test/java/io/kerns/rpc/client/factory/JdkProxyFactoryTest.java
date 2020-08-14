097db7f955edf624bc4e8d5ca9a0833ebb1b4e01
kk <realmall20@protonmail.com>
2020-08-14 10:45:47 +0800

    m

--- kerns-rpc-client/src/test/java/io/kerns/rpc/client/factory/JdkProxyFactoryTest.java
+++ kerns-rpc-client/src/test/java/io/kerns/rpc/client/factory/JdkProxyFactoryTest.java
@@ -9,9 +9,13 @@ public class JdkProxyFactoryTest {
     public static void main(String[] args) {
         System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
         JdkProxyFactory jdkProxyFactory = new JdkProxyFactory();
+        //设置序列化反序列化协议。
         jdkProxyFactory.setProtocol(new JavaSerializableMessageProtocol());
+        //设置服务发现
         jdkProxyFactory.setDiscover(new MockDiscover());
+        //设置网络交互客户端。
         jdkProxyFactory.setNetClient(new SocketNetClient());
+        //获取代理实现
         HelloInterface helloInterface = jdkProxyFactory.getProxy(HelloInterface.class);
         System.out.println(helloInterface.hello());
     }
